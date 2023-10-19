c3e9887f314ae0aa6957931b94848f539c09d5cb
funman300 <funman300@gmail.com>
2023-10-19 13:17:40 -0700

    new file:   hypr/configs/env.conf 	new file:   hypr/configs/exec.conf 	new file:   hypr/configs/keybinds.conf 	new file:   hypr/configs/windowsrules.conf 	new file:   hypr/hyprland.conf 	new file:   hypr/hyprpaper.conf 	new file:   hypr/scripts/dontkillsteam.sh 	new file:   hypr/scripts/waybar.sh 	new file:   nwg-bar/bar.json 	new file:   nwg-bar/style.css 	new file:   rofi/colors.rasi 	new file:   rofi/launcher.rasi 	new file:   rofi/launcher.sh 	new file:   rofi/powermenu.rasi 	new file:   rofi/powermenu.sh 	new file:   waybar/config 	new file:   waybar/style.css

---
+++ rofi/powermenu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ 
+lock=" Lock"
+logout=" Logout"
+shutdown="襤 Poweroff"
+reboot=" Reboot"
+sleep=" Suspend"
+ 
+selected_option=$(echo "$lock
+$logout
+$sleep
+$reboot
+$shutdown" | rofi -dmenu -i -p "Powermenu" \
+		  -theme "~/.config/rofi/powermenu.rasi")
+
+if [ "$selected_option" == "$shutdown" ]
+then
+  loginctl poweroff
+elif [ "$selected_option" == "$reboot" ]
+then
+  loginctl reboot
+elif [ "$selected_option" == "$sleep" ]
+then
+  loginctl suspend
+else
+  echo "No match"
+fi
